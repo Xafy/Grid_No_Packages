87663f40a982cbbde92c944b8556c64e2129a470
Xafy <57871519+Xafy@users.noreply.github.com>
2023-04-09 15:24:08 +0200

    .

--- src/app/services/fetch.service.ts
+++ src/app/services/fetch.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { DataInterface } from '../interfaces/data.interface';
 import { OptionsInterface } from '../interfaces/options.interface';
 
 const URL= "http://localhost:3000/api/info"
@@ -12,6 +11,7 @@ const URL= "http://localhost:3000/api/info"
 export class FetchService {
 
   constructor(private httpClient: HttpClient) { }
+  
   getData(options: OptionsInterface): Observable<any> {
     return this.httpClient.get(`${URL}?page=${options.page}&sort=${options.sort}&order=${options.order}&search=${options.searchValue}&size=${options.limit}`);
   }
